440ac13b241b19fd9f485b6da70dd3596ca5607e
SouhailLahlali <103116292+SouhailLahlali@users.noreply.github.com>
2023-08-04 15:26:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var settingsMenu=document.getElementById("settings");
+var darkBtn=document.getElementById("dark-btn");
+function settingsMenuToggle(){
+    if(settingsMenu.style.visibility=="visible")settingsMenu.style.visibility="hidden";
+    else settingsMenu.style.visibility="visible"   
+}
+darkBtn.onclick=function(){
+    darkBtn.classList.toggle("dark-btn-on")
+    document.body.classList.toggle("dark-theme")
+    if(localStorage.getItem("theme")=="light"){
+        localStorage.setItem("theme","dark")
+    }else{
+        localStorage.setItem("theme","light")
+    }
+}
+if(localStorage.getItem("theme")=="light"){
+    darkBtn.classList.remove("dark-btn-on")
+    document.body.classList.remove("dark-theme")
+}
+else if(localStorage.getItem("theme")=="dark"){
+    darkBtn.classList.add("dark-btn-on")
+    document.body.classList.add("dark-theme")
+}
+else{
+    localStorage.setItem("theme","dark");
+}
+
+
+
